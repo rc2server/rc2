7d2efa91906887738b2b55f1cab0dfdc2617efe2
Mark Lilback <mark@lilback.com>
2016-07-17 14:16:02 -0400

    fixed typos in build script

--- containers/buildall.sh
+++ containers/buildall.sh
@@ -4,8 +4,8 @@ cp ../rc2.sql dbserver/
 
 docker rmi dbserver 2>/dev/null
 (cd dbserver; docker build -t rc2/dbserver .)
-(cd appserver; wget http://192.168.1.5/rc2/rc2drop.jar; docker build -t rc2/appserver .)
-(cd compute; wget http://192.168.1.5/rc2/rc2compute.tar.gz ; docker build -t rc2/compute .)
+(cd appserver; wget http://192.168.1.5/rc2drop.jar; docker build -t rc2/appserver .)
+(cd compute; wget http://192.168.1.5/rc2compute.tar.gz ; docker build -t rc2/compute .)
 
 echo "build complete"
 

f97ac2219f5d6eb602b27d0d528c99ba284c37ad
Mark Lilback <mark@lilback.com>
2020-05-03 18:22:13 -0400

    updated so actually works with all values

--- documentation/evaluate-test.R
+++ documentation/evaluate-test.R
@@ -1,30 +1,32 @@
 library(evaluate)
 
+rm(foobar)
+tc <- textConnection("foobar", open = "w")
+
 createWrapper <- function() {
-  index <- 0
   curList <- list()
-  myEnv <- new.env()
+  myEnv <- new.env(parent = emptyenv())
+  assign("curValues", list(), envir = myEnv)
   assign("items", list(), envir = myEnv)
-#  items <- list()
-  assign("nextIndex", 1, envir = myEnv)
-  addGeneric <- function(obj) {
-    index <- get("nextIndex", envir = myEnv)
-    assign("nextIndex", index + 1, envir = myEnv)
-    curList = get("items", envir = myEnv)
-    curList[[index]] = obj
+  assign("nextValId", 1, envir = myEnv)
+  rc2evaluate <- function(src) {
+    rval <- evaluate(src, output_handler = outputHandler)
+    curList <- get("items", envir = myEnv)
+    curVals = get("curValues", envir = myEnv)
+    curList[["value"]] = structure((curVals))
     assign("items", curList, envir = myEnv)
-    invisible(NULL)
+    rval
   }
   outputHandler <- new_output_handler(
     source=function(value, visible = TRUE) {
-      index <- get("nextIndex", envir = myEnv)
-      assign("nextIndex", index + 1, envir = myEnv)
+      writeLines("source", con = tc)
       curList = get("items", envir = myEnv)
-      curList[[index]] = structure(list(data=value), myclass="src")
+      curList[['source']] = structure(list(data=value), myclass="src")
       assign("items", curList, envir = myEnv)
       invisible(NULL)
   }, 
   text=function(txt) {
+    writeLines("text", con = tc)
     # index <- get("nextIndex", envir = myEnv)
     # assign("nextIndex", index + 1, envir = myEnv)
     # curList = get("items", envir = myEnv)
@@ -33,39 +35,42 @@ createWrapper <- function() {
     # invisible(NULL)
   }, 
   message=function(msg) {
-    index <- get("nextIndex", envir = myEnv)
-    assign("nextIndex", index + 1, envir = myEnv)
+    writeLines("message", con = tc)
     curList = get("items", envir = myEnv)
-    curList[[index]] = structure(list(data=msg), myclass="message")
+    curList[['message']] = structure(list(data=msg), myclass="message")
     assign("items", curList, envir = myEnv)
   },
   warning=function(msg) {
-#    addGeneric(structure(list(data=msg), class="warning"))
   },
   error=function(msg) {
-    index <- get("nextIndex", envir = myEnv)
-    assign("nextIndex", index + 1, envir = myEnv)
+    writeLines("error", con = tc)
     curList = get("items", envir = myEnv)
-    curList[[index]] = structure(list(data=msg), myclass="myerror")
+    curList[['error']] = structure(list(data=msg), myclass="myerror")
     assign("items", curList, envir = myEnv)
   }, 
   graphics = function(data) {
 #    invisible()
   },
-  value=function(value, visible = TRUE) {
-    index <- get("nextIndex", envir = myEnv)
-    assign("nextIndex", index + 1, envir = myEnv)
-    curList = get("items", envir = myEnv)
-    curList[index] = structure(list(data=value, visible=visible), class="myvalue")
-    assign("items", curList, envir = myEnv)
-#    addGeneric(structure(list(value=value, visible=visible), class="value"))
-    invisible(NULL)
-  })
-  structure(list(outputHandler=outputHandler, env=myEnv)) #items=get("items", envir = myEnv)), class="wrapper")
+value=function(value, visible = TRUE) {
+  writeLines("value", con = tc)
+  vals <- structure(list(data=value, visible=visible), class="myvalue")
+  curVals <- get("curValues", envir = myEnv)
+  nextValId <- get("nextValId", envir = myEnv)
+  curVals[[nextValId]] = vals
+  assign("nextValId", nextValId + 1, envir = myEnv)
+  assign("curValues", curVals, envir = myEnv)
+  invisible(NULL)
+})
+  structure(list(outputHandler=outputHandler, env=myEnv, evaluate=rc2evaluate))
 }
 
 wrapper <- createWrapper()
-evaluate("2+2; doofus(); rnorm(10); message(\"hello\")", output_handler = wrapper$outputHandler, debug=FALSE)
+#if (TRUE) {
+#  dd <- evaluate("44-21; rnorm(10)", output_handler = wrapper$outputHandler, debug=TRUE)
+#  dd <- wrapper$evaluate("44-21; rnorm(10)")
+#} else {
+  wrapper$evaluate("2+2; doofus(); rnorm(10); message(\"hello\")")
+#}
 items <- get("items", envir = wrapper$env)
 items
 
